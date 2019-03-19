31269d808037466a71fab8ae0e7980fd4931c3bc
Marc Bolinas <bolinas.marc@gmail.com>
2019-03-19 18:24:01 -0400

    small fixes to export_poi_set()

--- preprocess.py
+++ preprocess.py
@@ -16,7 +16,7 @@
 
 ##this functions is going to have to take in a set of 'correct' POIs and the location that 
 ##the algorithm was run on
-def export_poi_set():
+def export_poi_set(road_name, poi_set):
 	result = open("./map/" + , "w");
 
 	res_f = open("./map/res_mapping_road_based.csv", "r")
@@ -25,4 +25,6 @@ def export_poi_set():
 		for name in poi_list:	#there's gotta be a way to perform an intersection that isn't O(n^2)
 			if items[0] == name:
 				#write to file
+
+	return;
 
