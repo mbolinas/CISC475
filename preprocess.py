685da6935f40109bea5df0f95ae706a2e1b66f1a
Marc Bolinas <bolinas.marc@gmail.com>
2019-03-19 18:24:58 -0400

    fixed compile error

--- preprocess.py
+++ preprocess.py
@@ -17,12 +17,12 @@
 ##this functions is going to have to take in a set of 'correct' POIs and the location that 
 ##the algorithm was run on
 def export_poi_set(road_name, poi_set):
-	result = open("./map/" + , "w");
+	result = open("./map/" + road_name, "w");
 
 	res_f = open("./map/res_mapping_road_based.csv", "r")
 	for s in res_f:
 		items = s.rstrip().split(',')
-		for name in poi_list:	#there's gotta be a way to perform an intersection that isn't O(n^2)
+		for name in poi_set:	#there's gotta be a way to perform an intersection that isn't O(n^2)
 			if items[0] == name:
 				#write to file
 
