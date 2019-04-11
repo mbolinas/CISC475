7bbe1964102b3dd2430db3b95fca47b3eba62694
Arvin Ay-ay <arvino@udel.edu>
2019-04-11 11:17:58 -0400

    instead of filtering out nodes with improper tags, code now only queries for correct tags

--- roadmapGeneration/intersectionGen.py
+++ roadmapGeneration/intersectionGen.py
@@ -16,23 +16,25 @@ import collections
 import csv
 
 api = overpy.Overpass()
-ouputFile = 'intersectionsNewark.csv'
+ouputFile = 'intersectionsTest.csv'
 
 '''
 Some bounding boxes (these are not exact since we are limited to
 specifying a box while most cities are obviously not shaped that way):
     Newark, DE: '(39.614244,-75.837469,39.712655,-75.742246)'
     Los Angeles, CA: '(33.702967, -118.669821, 34.338940, -118.152887)'
+    Tuscon, AZ: '(31.988275, -110.732295, 32.320246, -111.060301)'
     ...
     (need to add more)
 '''
 boundBox = '(39.614244,-75.837469,39.712655,-75.742246)'
+queryTags = "'highway'~'primary|secondary|residential|tertiary'"
 
 '''
 API Call
 Fetch all ways and nodes within a bounding box and store in 'result'.
 '''
-result = api.query("way{} ['highway'];(._;>;);out body;".format(boundBox))
+result = api.query("way{} [{}];(._;>;);out body;".format(boundBox, queryTags))
 
 '''
 Array of all nodeIDs in bounding box
@@ -70,15 +72,12 @@ duplicateIDs =\
 '''
 Write to CSV in form
     nodeID, tag, latitude, longitude
-Anything with a tag in filter will not be written
 '''
-filter = ['service', 'footway', 'motorway', 'motorway_link', 'trunk']
 with open(ouputFile,'wb') as file:
     # each id left in duplicateIDs will be an intersection
     for id in duplicateIDs:
-        if nodeInfo[id][1] not in filter:
-            file.write(nodeInfo[id][0].encode('utf-8') + ',')
-            file.write(nodeInfo[id][1].encode('utf-8') + ',')
-            file.write(str(nodeInfo[id][2]) + ',')
-            file.write(str(nodeInfo[id][3]))
-            file.write('\n')
+        file.write(nodeInfo[id][0].encode('utf-8') + ',')
+        file.write(nodeInfo[id][1].encode('utf-8') + ',')
+        file.write(str(nodeInfo[id][2]) + ',')
+        file.write(str(nodeInfo[id][3]))
+        file.write('\n')
