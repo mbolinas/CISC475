3e8dadeb83ff14e58d277c51f5f466a174e86e9d
Arvin Ay-ay <arvino@udel.edu>
2019-04-25 11:44:18 -0400

    workaround for yelp's request limit

--- map/poiGenYelp.py
+++ map/poiGenYelp.py
@@ -1,5 +1,5 @@
 """
-Adapted from Muhan's code
+Adapted from Muhan's auto_csv.py code
 
 This python file write the data into a csv file using a automatic way
 The file will have 50*(1+RANGE) samples.
@@ -14,9 +14,20 @@ import requests
 import sys
 import urllib
 import geopy.distance
+import time
 
-RANGE = 17 # how many pages to get
+RANGE = 50 # the max pages we try to get
 LIMIT = 50 # number of results from a single call; max 50,min 20
+# Categories to query. Full list here:
+    # https://www.yelp.com/developers/documentation/v3/all_category_list
+CATEGORIES =\
+    ['breakfast_brunch, All', 'chinese, All', 'buffets, All',\
+    'bars, All', 'nightlife, All', 'italian, All', 'japanese, All',\
+    'latin, All', 'steak, All', 'turkish, All', 'vietnamese, All',\
+    'vegan, All', 'vegetarian, All', 'sushi, All' , 'soup, All',\
+    'sandwiches, All', 'spanish, All', 'mexican, All', 'korean, All',\
+    'kebab, All', 'indpak, All', 'hotdog, All', 'filipino']
+SLEEP = 20 # how long to sleep between categories
 
 #Define the API Key, define the endpoint and define the header
 API_KEY = 'pdXJAlG5dNPgd3oqnypNJr1T7jihvqBzsrRMWPheuxR8IdN1CllWkLXaf0I6Y43igep_58Np2VFBQRwWEDCLYY_KnGZNfpKS8TeIYgV6wFX25xZVcx-LTbth9HyIXHYx'
@@ -33,8 +44,8 @@ ouputFile = './generated_map_data/poiYelp{}.csv'.format(lines[1].rstrip('\n'))
 location = lines[2].rstrip('\n')
 bBoxFile.close()
 
-print(boundBox)
-print(location)
+#print(boundBox)
+#print(location)
 
 '''
 Check if given coordinates are within the bounding box
@@ -47,30 +58,54 @@ def withinBoundBox(lat, lon):
     return a and b and c and d
 
 '''
-Writing to CSV
+Write a response to file
 '''
-with open(ouputFile, 'w') as writeFile:
-    OFFSET=1
-    for i in range(RANGE):
-        # Define the parameter for the first LIMIT samples
-        PARAMETER  = {'term' : 'restaurants',
-                     'limit' : LIMIT,
-                     'offset' : OFFSET,
-                     'location' : location}
-        # API Call
-        response = requests.get(url=ENDPOINT, params = PARAMETER, headers = HEADERS)
-        # store response
-        business_data = response.json()
-        # write in form address, lat, long, rating
-        for biz in business_data['businesses']:
-            address = biz['location']['address1']
-            latitude = biz['coordinates']['latitude']
-            longitude = biz['coordinates']['longitude']
-            rating = biz['rating']
-            row = address + "," + str(latitude) + "," + str(longitude) + "," + str(rating)+"\n"
+def writeResponse(business_data, file):
+    # write in form address, lat, long, rating
+    for biz in business_data['businesses']:
+        print(biz)
+        address = biz['location']['address1']
+        latitude = biz['coordinates']['latitude']
+        longitude = biz['coordinates']['longitude']
+        rating = biz['rating']
+        # sometimes businesses are missing specific info we need, so
+        # we check that it has all the necessary information
+        if all (k is not None for k in (latitude, longitude, address, rating)):
+            row =\
+                address.replace(',', '') + "," +\
+                str(latitude) + "," +\
+                str(longitude) + "," +\
+                str(rating)+"\n"
             # yelp's api is sometimes innacurate, returning values outside the specified location
             # for this reason we double check if a location is within our bounding box
             if withinBoundBox(latitude, longitude):
-                writeFile.write(row)
-        # set offset for next API call
-        OFFSET += LIMIT
+                file.write(row)
+
+'''
+Per the yelp FAQ, 'The API can only return up to 1,000 results at this time'
+To get around this, we query a category until it reaches the max results, sleep
+a bit, then move on to the next category
+'''
+with open(ouputFile, 'w') as writeFile:
+    for category in CATEGORIES:
+        OFFSET=0
+        for i in range(RANGE):
+            # Define the parameter API call
+            PARAMETER  = {'categories' : category,
+                         'limit' : LIMIT,
+                         'offset' : OFFSET,
+                         'location' : location}
+            # API Call
+            response = requests.get(url=ENDPOINT, params = PARAMETER, headers = HEADERS)
+            # if response is ok proceed writing to file
+            if response.status_code == 200:
+                business_data = response.json()
+                writeResponse(business_data, writeFile)
+                # set offset for next API call
+                OFFSET += LIMIT+1
+            # if response is bad, we've probably reached the last page of results and should break
+            elif response.status_code == 400:
+                print('End of category {}'.format(category))
+                print('Sleeping for {} seconds'.format(SLEEP))
+                time.sleep(SLEEP)
+                break
