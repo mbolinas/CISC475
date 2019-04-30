7bdf4d73db1b668854fbd1597dd5d5cfc586d813
anon <cbanning@udel.edu>
2019-04-30 14:20:23 -0400

    added in yelp edges

--- map/datasets/graphgen.py
+++ map/datasets/graphgen.py
@@ -1,22 +1,65 @@
 import csv
+from math import radians, cos, sin, asin, sqrt
 import networkx as nx
 import matplotlib.pyplot as plt
 
 
-def file_to_graph(filename):
-	data = open(filename, "r")
-	G = nx.read_edgelist(data, delimiter=',', nodetype=str, data=[('start_latitude',float),('start_longitude',float),('end_latitude',float),('end_longitude',float),('weight',float),('type',str)])
 
+#distance between two coordinates
+def haversine(lon1, lat1, lon2, lat2):
+    # convert decimal degrees to radians
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a))
+    r = 6371 # Radius of earth in kilometers. Use 3956 for miles
+    return c * r
+
+def file_to_graph(filename):
+        data = open(filename, "r")
+        G = nx.read_edgelist(data, delimiter=',', nodetype=str, data=[('start_latitude',float),('start_longitude',float),('end_latitude',float),('end_longitude',float),('weight',float)])
+
+        #set all nodes to intersection type
+        nodetype = []
+        nx.set_node_attributes(G, nodetype, 'type')
+        nodetype.append('intersection')
+        #for n in G.nodes(data=True):
+        #    print n
+        
 
         with open("res_mapping_road_based.csv") as res_csv:
                 reader = csv.reader(res_csv,delimiter=",")
 
+                edgenum = 9999
+                
                 for row in reader:
                         #format: [name,lat,long,maplat,maplong,rating,startnode,endnode]
                         #find edge with matching nodes, and append this node
                         rowData = G.get_edge_data(row[6],row[7])
-                        print rowData
-                        print row
+                        x1 = rowData["start_longitude"]
+                        y1 = rowData["start_latitude"]
+                        x2 = rowData["end_longitude"]
+                        y2 = rowData["end_latitude"]
+
+                        print "\nRoad segment: x1: " + str(x1) + " y1: " + str(y1) + " x2: " + str(x2) + " y2: " + str(y2)
+                        print "POI: lat: " + row[3] + " long: " + row[4]
+                        print "calcdist for segment: " + str(sqrt(pow((x2-x1),2) + pow((y2-y1),2)))
+                        print "list dist: " + str(rowData["weight"])
+                        print "haversine: " + str(haversine(x1,y1,x2,y2))
+                        print "haversine accuracy: " + str((rowData["weight"]-(rowData["weight"] - haversine(x1,y1,x2,y2)))/rowData["weight"]*100)
+                        #delete orgininal edge, make a new node and two new edges, one
+                        #to each original node from the new node
+                        #G.remove_edge(row[6],row[7])
+                        G.add_edge(row[6],edgenum,weight=haversine(x1,y1,float(row[4]),float(row[3])))
+                        G.add_edge(row[7],edgenum,weight=haversine(x2,y2,float(row[4]),float(row[3])))
+                        print "new dist: "
+                        print G.get_edge_data(row[6],edgenum)["weight"]
+                        print G.get_edge_data(row[7],edgenum)["weight"]
+                        print "total: " + str(G.get_edge_data(row[7],edgenum)["weight"] + G.get_edge_data(row[6],edgenum)["weight"])
+                        edgenum = edgenum + 1
 
 
         #for e in G.edges():
@@ -24,7 +67,7 @@ def file_to_graph(filename):
 
         nx.write_gpickle(G, 'graph_pickle')
 
-        
+
         #############################
         ### Old code for displaying image of the graph (needs some work with spacing, sizing, etc.)
         #############################
@@ -45,9 +88,9 @@ def file_to_graph(filename):
 filein = "road_segment.csv"
 
 if(filein == ""):
-	print("Please specify a file to read in (modify this file or call file_to_graph(filename) )")
+        print("Please specify a file to read in (modify this file or call file_to_graph(filename) )")
 else:
-	file_to_graph(filein)
+        file_to_graph(filein)
 
 
 '''
