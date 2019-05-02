aabfc09f45d15c51bf9a16fde71c854dc31e8f7d
anon <cbanning@udel.edu>
2019-05-02 18:05:25 -0400

    new mapgraph generation function

--- map/datasets/graphgen.py
+++ map/datasets/graphgen.py
@@ -18,8 +18,8 @@ def haversine(lon1, lat1, lon2, lat2):
     r = 6371 # Radius of earth in kilometers. Use 3956 for miles
     return c * r
 
-def file_to_graph(filename):
-        data = open(filename, "r")
+def file_to_graph(roadSegmentFilename, poiFilename):
+        data = open(roadSegmentFilename, "r")
         G = nx.read_edgelist(data, delimiter=',', nodetype=str, data=[('start_latitude',float),('start_longitude',float),('end_latitude',float),('end_longitude',float),('weight',float)])
 
         #set all nodes to intersection type
@@ -30,7 +30,7 @@ def file_to_graph(filename):
         #    print n
         
 
-        with open("res_mapping_road_based.csv") as res_csv:
+        with open(poiFilename) as res_csv:
                 reader = csv.reader(res_csv,delimiter=",")
 
                 edgenum = 9999
@@ -68,29 +68,71 @@ def file_to_graph(filename):
         nx.write_gpickle(G, 'graph_pickle')
 
 
-        #############################
-        ### Old code for displaying image of the graph (needs some work with spacing, sizing, etc.)
-        #############################
 
-        #pos = nx.spring_layout(G,scale=1)
+def file_to_graph2(roadSegmentFilename, poiFilename):
+    G = nx.Graph()
+    # a rewrite of file_to_graph
+    with open(roadSegmentFilename) as rseg:
+        #format: [node1,node2,lat1,long1,lat2,long2,weight]
+        reader = csv.reader(rseg,delimiter=",")
+        for row in reader:
+            node1 = row[0]
+            node2 = row[1]
+            lat1 = row[2]
+            long1 = row[3]
+            lat2 = row[4]
+            long2 = row[5]
+            length = row[6]
+            G.add_node(node1,latitude=lat1,longitude=long1,nodetype="intersection")
+            G.add_node(node2,latitude=lat2,longitude=long2,nodetype="intersection")
+            G.add_edge(node1,node2,weight=length)
 
-        #node_labels = nx.get_node_attributes(G,'state')
-        #nx.draw_networkx_labels(G, pos, labels = node_labels)
-        #edge_labels = nx.get_edge_attributes(G,'weight')
-        #nx.draw_networkx_edge_labels(G, pos, labels = edge_labels)
+#    for e in G.edges(data=True):
+#        print e
 
-        #nx.draw(G,pos, with_labels=True)
-        #plt.savefig('this.png')
-        #plt.show()
-        ############################################################
+#    for n in G.nodes(data=True):
+#        print n
+            
+    with open(poiFilename) as pois:
+        #format: [name,lat,long,maplat,maplong,rating,startnode,endnode]
+        #the lat and long is the real lat and long, while the mapping lat and long is
+        #the place along an actual segment which it is assigned to.
+        reader = csv.reader(pois,delimiter=",")
+        for row in reader:
+            #poi data
+            name = row[0]
+            lat = row[1]
+            lng = row[2]
+            maplat = row[3]
+            maplng = row[4]
+            rate = row[5]
+            node1 = row[6]
+            node2 = row[7]
+            #data for endpoints of poi's road segment. has nodetype, latitude, and longitude
+            node1Data = G.node[node1]
+            node2Data = G.node[node2]
+
+            #add poi as node
+            G.add_node(name,latitude=maplat,longitude=maplng,reallat=lat,reallong=lng,rating=rate,nodetype="poi")
+            #add edge to each endpoint
+            G.add_edge(node1,name,weight=haversine(float(node1Data['longitude']),float(node1Data['longitude']),float(maplng),float(maplat)))
+            G.add_edge(node2,name,weight=haversine(float(node2Data['longitude']),float(node2Data['longitude']),float(maplng),float(maplat)))
+
+        
+    for e in G.edges(data=True):
+        print e
+    for n in G.nodes(data=True):
+        print n
+    nx.write_gpickle(G, 'graph_pickle2')
 
 #road segment file (start node, end node, start_lat, start_long, end_lat, end long, weight/distance)
 filein = "road_segment.csv"
-
+poifilein = "res_mapping_road_based.csv"
 if(filein == ""):
         print("Please specify a file to read in (modify this file or call file_to_graph(filename) )")
 else:
-        file_to_graph(filein)
+        file_to_graph(filein, poifilein)
+        file_to_graph2(filein, poifilein)
 
 
 '''
