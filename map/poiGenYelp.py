302f5fdfaab9d1b33e7d7f4d24c7e277e3e9c935
Arvin Aya-ay <arvino@udel.edu>
2019-05-24 21:34:21 +0000

    added code to write distance ratio and price values of poi's

--- map/poiGenYelp.py
+++ map/poiGenYelp.py
@@ -8,8 +8,8 @@ This file will depend on a roadSegmentXXX.csv corresponding
 to the area you want to generate poi's for. Please generate that using
 roadSegmentGen.py before attempting to use this file.
 
-Generated start/end node id's are still rough with some poi's given id's that
-are not accurate. I'm still thinking of ways to get the most accurate id's
+Generated start/end node id's are still rough with some poi's not mapped to the
+correct road segment. I'm still thinking of ways to get the most accurate id's
 
 Expect long runtimes
 '''
@@ -17,7 +17,6 @@ import requests
 import sys
 import geopy.distance
 import time
-#from difflib import SequenceMatcher
 
 #Define the API Key, define the endpoint and define the header
 API_KEY = 'pdXJAlG5dNPgd3oqnypNJr1T7jihvqBzsrRMWPheuxR8IdN1CllWkLXaf0I6Y43igep_58Np2VFBQRwWEDCLYY_KnGZNfpKS8TeIYgV6wFX25xZVcx-LTbth9HyIXHYx'
@@ -59,7 +58,8 @@ ABBRV_OTHER = {\
     }
 
 SLEEP = 20 # how long to sleep between categories
-MATCH_RATIO = .97 # minimum match ratio
+
+POI_ID = 0 # global variable to assign unique id's to poi's
 # --------------------------------------------------------------------------
 
 #--------------------------parse config file--------------------------------
@@ -69,7 +69,7 @@ lines = bBoxFile.readlines()
 name = lines[1].rstrip('\n')
 
 # Bounding box used for computation
-BOUND_BOX = lines[0].replace('(','').replace(')','').rstrip('\n').split(',') # lol i'll fix later
+BOUND_BOX = lines[0].replace('(','').replace(')','').rstrip('\n').split(',')
 
 # getting road segments for start/end id computation
 roadSegmentInput = "./generated_map_data/roadSegments{}.csv".format(name)
@@ -135,22 +135,6 @@ def expand_abbrv(address):
     return expanded
 
 '''
-FUNCTION NOT CURRENTLY BEING USED
-Input
-    segment_array : array of road segments
-Output
-    Returns an array of road segments who's names match the given address
-'''
-def filter_names(address,segment_array):
-    poi_street_name = expand_abbrv(address)
-    arr = []
-    for segment in segment_array:
-        s = SequenceMatcher(None, segment[7], poi_street_name)
-        if s.ratio() > MATCH_RATIO:
-            arr.append(segment)
-    return arr
-
-'''
 Input
     lat/lon : float lat/lon coordinates
     segment_array : array of road segments
@@ -176,6 +160,21 @@ def get_immediate_segments(lat, long, segment_dict):
 '''
 Input
     lat/lon : float lat/lon coordinates
+    segment : a road segment
+Output
+    The ratio of the distance of the given coordinates on the road segment
+'''
+def calculateRatio(lat, long, segment):
+    distance_APoi =\
+        geopy.distance.distance((lat, long), (segment[2], segment[3])).km
+    distance_BPoi =\
+        geopy.distance.distance((lat, long), (segment[4], segment[5])).km
+
+    return distance_APoi / (distance_APoi + distance_BPoi)
+
+'''
+Input
+    lat/lon : float lat/lon coordinates
     address : a string
 Output
     returns the closest road segment to a given lat/long coordinate.
@@ -186,21 +185,28 @@ def get_closest_segment(lat, long, address, segment_dict):
     address_expanded = expand_abbrv(address)
     immediate_segments = []
 
+    # attempt to use address matching
     if address_expanded in segment_dict:
         immediate_segments = segment_dict[address_expanded]
 
+    # if address matching fails, use get_immediate_segments()
     if len(immediate_segments) == 0:
         immediate_segments = get_immediate_segments(lat, long, segment_dict)
 
-    #print(immediate_segments)
     # find closest segment out of immediate segments
     for segment in immediate_segments:
-        distance =\
-            geopy.distance.distance((lat, long), (segment[2], segment[3])).km
-        if distance < segment[6]:
+        #print(segment[7])
+        '''
+        if (lat > segment[2] and lat < segment[4] and lon > segment[3] and lon < segment[5])\
+            or (lat < segment[2] and lat > segment[4] and lon < segment[3] and lon > segment[5]):
+            return segment
+        '''
+        distanceA = geopy.distance.distance((lat, long), (segment[2], segment[3])).km
+        distanceB = geopy.distance.distance((lat, long), (segment[4], segment[5])).km
+        if distanceA < segment[6] and distanceB < segment[6]:
             return segment
 
-    return immediate_segments[0]
+    return None
 
 '''
 Input
@@ -210,28 +216,45 @@ Output
     No return value, writes output to file
 '''
 def writeResponse(response, file, segment_dict):
+    global POI_ID
     business_data = response.json()
     for biz in business_data['businesses']:
         address = biz['location']['address1']
         latitude = biz['coordinates']['latitude']
         longitude = biz['coordinates']['longitude']
         rating = biz['rating']
-        print(address)
+        price = None
+        if 'price' in biz:
+            price = biz['price']
         # sometimes businesses are missing specific info we need, so
         # we check that it has all the necessary information
-        if all (k is not None for k in (address, latitude, longitude, rating)) and len(address.split()) > 2:
+        if all (k is not None for k in (address, latitude, longitude, rating, price)) and len(address.split()) > 2:
+            # road segment the poi is on
             segment = get_closest_segment(latitude, longitude, address, segment_dict)
-            row =\
-                address.encode("utf-8").replace(',', '') + "," +\
-                str(latitude) + "," +\
-                str(longitude) + "," +\
-                str(rating) + "," +\
-                str(segment[0]) + "," +\
-                str(segment[1]) + "\n"
-            # yelp's api is sometimes innacurate, returning values outside the specified location
-            # for this reason we double check if a location is within our bounding box
-            if withinBoundBox(latitude, longitude):
-                file.write(row)
+            # ratio of poi's distance on road segment
+            if segment is not None:
+                ratio = calculateRatio(latitude, longitude, segment)
+
+                row =\
+                    str(POI_ID) + "," +\
+                    address.encode("utf-8").replace(',', '') + "," +\
+                    str(latitude) + "," +\
+                    str(longitude) + "," +\
+                    str(rating) + "," +\
+                    price.encode("utf-8") + "," +\
+                    str(segment[0]) + "," +\
+                    str(segment[1]) + "," +\
+                    str(ratio) + "\n"
+                print(row)
+                # yelp's api is sometimes innacurate, returning values outside the specified location
+                # for this reason we double check if a location is within our bounding box
+                if withinBoundBox(latitude, longitude):
+                    # increment id
+                    POI_ID += 1
+                    try:
+                        file.write(row)
+                    except Exception as e:
+                        print(e)
 
 def main():
     start_time = time.time()
@@ -251,8 +274,7 @@ def main():
                          'location' : LOCATION}
             # API Call
             print('Querying category "{}" from {} to {}...'.format(category, offset, offset+RANGE))
-            response =\
-                requests.get(url=ENDPOINT, params=PARAMETER, headers=HEADERS)
+            response = requests.get(url=ENDPOINT, params=PARAMETER, headers=HEADERS)
 
             # if response is ok proceed writing to file
             if response.status_code == 200:
